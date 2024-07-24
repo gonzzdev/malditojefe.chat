29cdf9e837c7afaf5405382cb0a5b3030b17f6cd
Daniel Herrera <dani122pumas@gmail.com>
2024-07-24 10:44:17 -0600

    Empty chievement component add

---
+++ components/achievements/page.tsx
@@ -0,0 +1,15 @@
+
+export default function EmptyAchievementComponent() {
+    return (
+        <main>
+            <div className="bg-custom-gradient-empty-achievement w-1/6 h-96 border-4 border-red-800">
+
+            </div>
+            <div className=" h-10 w-1/6">
+                <p className="text-center text-red-600">
+                    Logro no encontrado
+                </p>
+            </div>
+        </main>
+    );
+}
