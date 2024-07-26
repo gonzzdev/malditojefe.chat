94bcd31a34d57305cad73743821072c02796d145
Bryan Rosas <bryan.rosas.quispe@gmail.com>
2024-07-25 23:37:42 -0500

    feat: Añade background de jefe y fuego, agrega tambien imagenes y input chat

---
+++ app/game/page.tsx
@@ -0,0 +1,33 @@
+import Image from "next/image";
+import {Input} from "@/components/ui/input";
+import {InputChat} from "@/components/chat/input-chat";
+
+export default function GamePage() {
+    return (
+        <main className="relative w-full h-[calc(100vh-4rem)]">
+            <div className="absolute bottom-0 max-w-[620px] md:w-full">
+                <Image alt="image del jefe la ilustracion" src="/static/images/game/jefe.png"
+                       width={0} height={0} sizes="100vw"
+                       style={{width: 'auto', height: 'auto'}}
+                />
+            </div>
+            <div className="relative w-full h-full flex justify-center items-end z-10 py-10">
+                <div>
+
+                </div>
+                <div>
+                    <InputChat disabled={false}></InputChat>
+                </div>
+            </div>
+            <div className="absolute bottom-0 flex justify-center w-full">
+                <div className="max-w-[60rem] md:w-full flex justify-center">
+                    <Image alt="image del jefe la ilustracion" src="/static/images/game/fuego.png"
+                           width={0} height={0} sizes="100vw"
+                           className="align-middle"
+                           style={{width: 'auto', height: 'auto'}}
+                    />
+                </div>
+            </div>
+        </main>
+    )
+}
