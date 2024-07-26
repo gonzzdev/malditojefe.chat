148cf7de49ff704671f788c22c970c240fc8d49d
José  Gonzz <streamofficemx@gmail.com>
2024-07-26 15:15:37 -0600

    Solo revisión

--- app/game/page.tsx
+++ app/game/page.tsx
@@ -5,7 +5,7 @@ import {InputChat} from "@/components/chat/input-chat";
 export default function GamePage() {
     return (
         <main className="relative w-full h-[calc(100vh-4rem)]">
-            <div className="absolute bottom-0 max-w-[620px] md:w-full">
+            <div className="absolute bottom-0 max-w-[500px] md:w-full">
                 <Image alt="image del jefe la ilustracion" src="/static/images/game/jefe.png"
                        width={0} height={0} sizes="100vw"
                        style={{width: 'auto', height: 'auto'}}
@@ -21,7 +21,7 @@ export default function GamePage() {
             </div>
             <div className="absolute bottom-0 flex justify-center w-full">
                 <div className="max-w-[60rem] md:w-full flex justify-center">
-                    <Image alt="image del jefe la ilustracion" src="/static/images/game/fuego.png"
+                    <Image alt="Imagen de flama" src="/static/images/game/fuego.png"
                            width={0} height={0} sizes="100vw"
                            className="align-middle"
                            style={{width: 'auto', height: 'auto'}}
