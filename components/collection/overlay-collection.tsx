36e3472e2f04744a8ac2bfe03aaa8d85dd2d0045
Bryan Rosas <bryan.rosas.quispe@gmail.com>
2024-08-01 21:47:19 -0500

    fix: Corrige responsive

--- components/collection/overlay-collection.tsx
+++ components/collection/overlay-collection.tsx
@@ -6,7 +6,7 @@ export const OverlayCollection = () => {
     const {achievement, isShow} = useAchievements();
 
     return (
-        <div className="absolute right-10 top-10">
+        <div className="absolute right-10 top-10 z-20">
             {isShow && <CardCollection url={achievement!.url} name={achievement!.name} isAchievement={true}/>}
         </div>
     )
