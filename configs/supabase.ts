d35fc6133f2c721dbde0bbd79e25a99d4fa9f34c
Gonzz <streambytemx@gmail.com>
2024-08-06 17:22:17 -0700

    commit

--- configs/supabase.ts
+++ configs/supabase.ts
@@ -1,6 +1,6 @@
 import {createClient} from '@supabase/supabase-js'
 
-const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL = 'https://vlbegpizawhfbxerbeyu.supabase.co/'
-const SUPABASE_KEY = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZsYmVncGl6YXdoZmJ4ZXJiZXl1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjI2Mjk3ODIsImV4cCI6MjAzODIwNTc4Mn0.GwzcD7wk88SycjEIaDNqmqxjaFDb8kq6-xHluwA38eE'
+const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL = 
+const SUPABASE_KEY = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY = 
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY)
